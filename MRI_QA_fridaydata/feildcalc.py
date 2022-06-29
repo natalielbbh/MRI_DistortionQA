8eb05745d41f8e4517e29af72deb1ea78c84b830
Natalie Gironda <107661826+natalielbbh@users.noreply.github.com>
2022-06-29 15:32:40 +1000

    working on spherical harmonics comparison in three planes

--- MRI_QA_fridaydata/feildcalc.py
+++ MRI_QA_fridaydata/feildcalc.py
@@ -9,5 +9,33 @@ data_loc = Path('/Users/nataliegironda/Desktop/20220624QAQA')
 matched_volume = pd.read_csv('Matched_Markers.csv', index_col=0).squeeze("columns")
 dicom_data_loc = data_loc / '02 gre_trans_AP_330' / 'Original' / 'dicom_data.json'  # previosly saved from a MarkerVolume using save_dicom_data()
 
-Bz_field = ConvertMatchedMarkersToBz(matched_volume, dicom_data_loc)
+matched_volume1 = pd.read_csv('Matched_Markers1.csv', index_col=0).squeeze("columns")
+dicom_data_loc1 = data_loc / '03 gre_trans_PA_330' / 'Original' / 'dicom_data.json'
+
+matched_volume2 = pd.read_csv('Matched_Markers2.csv', index_col=0).squeeze("columns")
+dicom_data_loc2 = data_loc / '04 gre_sag_AP_330' / 'Original' / 'dicom_data.json'
+
+matched_volume3 = pd.read_csv('Matched_Markers3.csv', index_col=0).squeeze("columns")
+dicom_data_loc3 = data_loc / '05 gre_sag_PA_330' / 'Original' / 'dicom_data.json'
+
+matched_volume4 = pd.read_csv('Matched_Markers4.csv', index_col=0).squeeze("columns")
+dicom_data_loc4 = data_loc / '06 gre_cor_RL_330' / 'Original' / 'dicom_data.json'
+
+
+Bz_field =ConvertMatchedMarkersToBz(matched_volume, dicom_data_loc)
 Bz_field.MagneticFields.to_csv('Bfields.csv')  # save for later
+
+Bz_field = ConvertMatchedMarkersToBz(matched_volume1, dicom_data_loc1)
+Bz_field.MagneticFields.to_csv('Bfields1.csv')  # save for later
+
+Bz_field = ConvertMatchedMarkersToBz(matched_volume2, dicom_data_loc2)
+Bz_field.MagneticFields.to_csv('Bfields2.csv')  # save for later
+
+Bz_field = ConvertMatchedMarkersToBz(matched_volume3, dicom_data_loc3)
+Bz_field.MagneticFields.to_csv('Bfields3.csv')  # save for later
+
+Bz_field = ConvertMatchedMarkersToBz(matched_volume4, dicom_data_loc4)
+Bz_field.MagneticFields.to_csv('Bfields4.csv')  # save for later
+
+
+
--- MRI_QA_fridaydata/spherical_harmonics.py
+++ MRI_QA_fridaydata/spherical_harmonics.py
@@ -19,3 +19,53 @@ G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(
 G_x_Harmonics.harmonics.to_csv('G_X_Harmonics.csv')
 G_y_Harmonics.harmonics.to_csv('G_y_Harmonics.csv')
 G_z_Harmonics.harmonics.to_csv('G_z_Harmonics.csv')
+
+
+FieldData = pd.read_csv('Bfields1.csv', index_col=0).squeeze("columns")
+dicom_data_loc = Path('/Users/nataliegironda/Desktop/20220624QAQA/03 gre_trans_PA_330/Original/dicom_data.json' )# previosly saved from a MarkerVolume
+dicom_data = get_dicom_data(dicom_data_loc)
+gradient_strength = dicom_data['gradient_strength']
+normalisation_factor = [1/gradient_strength[0], 1/gradient_strength[1], 1/gradient_strength[2], 1]  # this normalised gradient harmonics to 1mT/m
+
+G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(FieldData, norm=normalisation_factor, n_order=8)
+# note that B0_harmonics is None as we did not provide distorted_volume_rev to calculate_harmonics
+G_x_Harmonics.harmonics.to_csv('1G_X_Harmonics.csv')
+G_y_Harmonics.harmonics.to_csv('1G_y_Harmonics.csv')
+G_z_Harmonics.harmonics.to_csv('1G_z_Harmonics.csv')
+
+FieldData = pd.read_csv('Bfields2.csv', index_col=0).squeeze("columns")
+dicom_data_loc = Path('/Users/nataliegironda/Desktop/20220624QAQA/04 gre_sag_AP_330/Original/dicom_data.json' )# previosly saved from a MarkerVolume
+dicom_data = get_dicom_data(dicom_data_loc)
+gradient_strength = dicom_data['gradient_strength']
+normalisation_factor = [1/gradient_strength[0], 1/gradient_strength[1], 1/gradient_strength[2], 1]  # this normalised gradient harmonics to 1mT/m
+
+G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(FieldData, norm=normalisation_factor, n_order=8)
+# note that B0_harmonics is None as we did not provide distorted_volume_rev to calculate_harmonics
+G_x_Harmonics.harmonics.to_csv('2G_X_Harmonics.csv')
+G_y_Harmonics.harmonics.to_csv('2G_y_Harmonics.csv')
+G_z_Harmonics.harmonics.to_csv('2G_z_Harmonics.csv')
+
+FieldData = pd.read_csv('Bfields3.csv', index_col=0).squeeze("columns")
+dicom_data_loc = Path('/Users/nataliegironda/Desktop/20220624QAQA/05 gre_sag_PA_330/Original/dicom_data.json' )# previosly saved from a MarkerVolume
+dicom_data = get_dicom_data(dicom_data_loc)
+gradient_strength = dicom_data['gradient_strength']
+normalisation_factor = [1/gradient_strength[0], 1/gradient_strength[1], 1/gradient_strength[2], 1]  # this normalised gradient harmonics to 1mT/m
+
+G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(FieldData, norm=normalisation_factor, n_order=8)
+# note that B0_harmonics is None as we did not provide distorted_volume_rev to calculate_harmonics
+G_x_Harmonics.harmonics.to_csv('3G_X_Harmonics.csv')
+G_y_Harmonics.harmonics.to_csv('3G_y_Harmonics.csv')
+G_z_Harmonics.harmonics.to_csv('3G_z_Harmonics.csv')
+
+FieldData = pd.read_csv('Bfields4.csv', index_col=0).squeeze("columns")
+dicom_data_loc = Path('/Users/nataliegironda/Desktop/20220624QAQA/06 gre_cor_RL_330/Original/dicom_data.json' )# previosly saved from a MarkerVolume
+dicom_data = get_dicom_data(dicom_data_loc)
+gradient_strength = dicom_data['gradient_strength']
+normalisation_factor = [1/gradient_strength[0], 1/gradient_strength[1], 1/gradient_strength[2], 1]  # this normalised gradient harmonics to 1mT/m
+
+G_x_Harmonics, G_y_Harmonics, G_z_Harmonics, B0_Harmonics = calculate_harmonics(FieldData, norm=normalisation_factor, n_order=8)
+# note that B0_harmonics is None as we did not provide distorted_volume_rev to calculate_harmonics
+G_x_Harmonics.harmonics.to_csv('4G_X_Harmonics.csv')
+G_y_Harmonics.harmonics.to_csv('4G_y_Harmonics.csv')
+G_z_Harmonics.harmonics.to_csv('4G_z_Harmonics.csv')
+
